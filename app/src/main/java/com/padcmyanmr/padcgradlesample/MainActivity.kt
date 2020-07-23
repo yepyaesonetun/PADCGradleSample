c1224837f6d3bee2d02a83315f840e0992986840
yepyaesonetunMMDS <yepyaesone.tun@m-digital-solutions.com>
2020-07-23 17:55:42 +0630

    shared module added.

--- app/src/main/java/com/padcmyanmr/padcgradlesample/MainActivity.kt
+++ app/src/main/java/com/padcmyanmr/padcgradlesample/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.padcmyanmr.padcgradlesample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.padcmyanmr.shared.BaseActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ shared/src/main/java/com/padcmyanmr/shared/BaseActivity.kt
@@ -0,0 +1,11 @@
+package com.padcmyanmr.shared
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Created by Ye Pyae Sone Tun
+ * on 23/07/2020.
+ */
+
+abstract class BaseActivity : AppCompatActivity() {
+}
